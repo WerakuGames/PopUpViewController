9482be0d774ae39c8ea9f8590cdff028c0e3fa2f
Ahmad Ashraf Azman <Ashraf@Ahmads-MacBook-Pro.local>
2015-09-21 19:11:11 +0800

    Initial Commit

---
+++ TestProduct/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProduct
+//
+//  Created by Ahmad Ashraf Azman on 9/21/15.
+//  Copyright (c) 2015 Ahmad Ashraf Azman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestProduct/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestProduct
+//
+//  Created by Ahmad Ashraf Azman on 9/21/15.
+//  Copyright (c) 2015 Ahmad Ashraf Azman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
